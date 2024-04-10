473354cc58d69ac3e5579652a957cd0e46dda56f
Gokul Alex <gokulsan@live.com>
2024-04-11 00:56:43 +0530

    Initial Deployment of SPL Token

--- src/native_mint.rs
+++ src/native_mint.rs
@@ -4,7 +4,7 @@
 pub const DECIMALS: u8 = 9;
 
 // The Mint for native SOL Token accounts
-solana_program::declare_id!("NALA");
+solana_program::declare_id!("So11111111111111111111111111111111111111112");
 
 #[cfg(test)]
 mod tests {
